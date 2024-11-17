59a2e220de65a1273c435b8f5ec86854267d41c0
troym18 <troymcbride1804@gmail.com>
2024-11-16 21:44:25 -0500

    V1.0

--- tp.py
+++ tp.py
@@ -1,4 +1,11 @@
 from cmu_graphics import *
+def onAppStart(app):
+    app.width=900
+    app.height=600
 def redrawAll(app):
-    drawCircle(100,100,50)
+    drawMap(app)
+
+def drawMap(app):
+    drawRect(50,50,800,500,fill=None,border='black',borderWidth=5)
+    drawArc(75,75,50,50,90,90,fill=None,border='black',borderWidth=5)
 runApp()
