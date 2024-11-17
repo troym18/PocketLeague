6f0865c119ba4048b11c7a405593cfcb9e1b61c0
troym18 <troymcbride1804@gmail.com>
2024-11-16 23:31:33 -0500

    v1.0

---
+++ player.py
@@ -0,0 +1,7 @@
+class Player:
+    def __init__(self,team):
+        self.px=0
+        self.py=0
+        self.vx=0
+        self.vy=0
+        self.team=team
--- tp.py
+++ tp.py
@@ -1,11 +1,14 @@
 from cmu_graphics import *
 def onAppStart(app):
-    app.width=900
-    app.height=600
+    app.width=1200
+    app.height=800
+
 def redrawAll(app):
     drawMap(app)
 
 def drawMap(app):
-    drawRect(50,50,800,500,fill=None,border='black',borderWidth=5)
-    drawArc(75,75,50,50,90,90,fill=None,border='black',borderWidth=5)
+    drawRect(25,75,app.width-50,app.height-150,fill=None,border='black',borderWidth=5)
+    drawLine(25,300,25,500,fill='blue',lineWidth=15)
+    drawLine(app.width-25,300,app.width-25,500,fill='orange',lineWidth=15)
+    drawLine(app.width/2-10,app.height-75,app.width/2+10,app.height-75,fill='red',lineWidth=10)
 runApp()
